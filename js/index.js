a3585e284decfaa5ee8fead34fc06ab8aecb30a2
John Purviance <ffdd4846@vt.edu>
2016-07-30 19:18:32 -0500

    changed cols and fixed resume link

--- js/index.js
+++ js/index.js
@@ -3,12 +3,12 @@
  $(document).ready(function () {
    if ($(window).width() > 1000) {
 
-     $("#VT-Hacks").children(".col-md-4").each(function () {
+     $("#VT-Hacks").children(".col-lg-4").each(function () {
        if ($(this).height() > max) {
          max = $(this).height()
        }
      });
-     $("#VT-Hacks").children(".col-md-4").each(function () {
+     $("#VT-Hacks").children(".col-lg-4").each(function () {
        $(this).height(max);
      });
 
@@ -17,13 +17,13 @@
        if ($(this).width() != width) {
          max = 0;
          width = $(this).width();
-         $("#VT-Hacks").children(".col-md-4").each(function () {
+         $("#VT-Hacks").children(".col-lg-4").each(function () {
            $(this).height(0);
            if ($(this).height() > max) {
              max = $(this).height()
            }
          });
-         $("#VT-Hacks").children(".col-md-4").each(function () {
+         $("#VT-Hacks").children(".col-lg-4").each(function () {
            $(this).height(max);
          });
        }
@@ -32,12 +32,12 @@
 
    if ($(window).width() > 1000) {
 
-     $("#CafeConnect").children(".col-md-4").each(function () {
+     $("#CafeConnect").children(".col-lg-4").each(function () {
        if ($(this).height() > max_2) {
          max_2 = $(this).height()
        }
      });
-     $("#CafeConnect").children(".col-md-4").each(function () {
+     $("#CafeConnect").children(".col-lg-4").each(function () {
        $(this).height(max_2);
      });
 
@@ -46,13 +46,13 @@
        if ($(this).width() != width_2) {
          max_2 = 0;
          width_2 = $(this).width();
-         $("#CafeConnect").children(".col-md-4").each(function () {
+         $("#CafeConnect").children(".col-lg-4").each(function () {
            $(this).height(0);
            if ($(this).height() > max_2) {
              max_2 = $(this).height()
            }
          });
-         $("#CafeConnect").children(".col-md-4").each(function () {
+         $("#CafeConnect").children(".col-lg-4").each(function () {
            $(this).height(max_2);
          });
        }
